f3725eb4cf9079ae4ec1c8f84630acb4ed98dcf8
stevensc1238 <cole.c.stevenson@outlook.com>
2025-04-05 23:08:58 -0400

    Add files via upload
    First Submission

---
+++ P1LAB1_StevensonCole.py
@@ -0,0 +1,11 @@
+#Cole Stevenson
+#04/5/2025
+#P1LAB1
+#Create a Python program using IDLE to get user info and print
+
+#Get user info
+first_name = input('Enter your first name: ')
+last_name = input('Enter your last name: ')
+
+#Print collected info
+print(f'Hello, {first_name} {last_name}! Welcome to CTI-110')
