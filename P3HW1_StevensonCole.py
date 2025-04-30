933518b1b5f9e7bced74b7035d332f75e4504950
stevensc1238 <stevensc1238@student.faytechcc.edu>
2025-04-30 12:13:40 -0400

    First Submission

---
+++ P3HW1_StevensonCole.py
@@ -0,0 +1,54 @@
+#StevensonCole
+#4/23/2025
+#P3HW1
+#Debug!
+
+
+# This program takes a number grade , determines average and displays letter grade for average.
+
+# Enter grades for six modules
+
+mod_1 = float(input('Enter grade for Module 1: '))
+mod_2 = float(input('Enter grade for Module 2: '))
+mod_3 = float(input('Enter grade for Module 3: '))
+mod_4 = float(input('Enter grade for Module 4: '))
+mod_5 = float(input('Enter grade for Module 5: '))
+mod_6 = float(input('Enter grade for Module 6: '))
+
+# add grades entered to a list
+
+grades = [mod_1, mod_2, mod_3, mod_4, mod_5, mod_6]
+
+# TO DO: determine lowest, highest , sum and average for grades
+
+low = min(grades)
+high = max(grades)
+total = sum(grades)
+avg = total / len(grades)
+print(f"\n------------Results------------")
+print(f"Lowest grade: {low}")
+print(f"Highest grade: {high}")
+print(f"Total of grades: {total}")
+print(f"Average grade: {avg:.2f}")
+print(f"-----------------------------------")
+
+# determine letter grade for average
+
+
+if avg >= 90:
+    print('Your grade is: A')
+elif avg >= 80:
+    print('Your grade is: B')
+elif avg >= 70:
+    print('Your grade is: C')
+elif avg >= 60:
+    print('Your grade is: D')
+else:
+    print('Your grade is: F')
+
+# TO DO: finish this
+
+
+
+
+
